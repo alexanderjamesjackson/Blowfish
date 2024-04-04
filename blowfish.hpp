bf299cd1c6409798cd5fbb7cd2baa6e56413a0ff
j44ck00 <alex.jackson@new.ox.ac.uk>
2024-04-04 16:57:16 +0100

    Added interface to blowfish class, implementation of F-Function and partial inititialisation of P-array

---
+++ blowfish.hpp
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <vector>
+using namespace std;
+
+class Blowfish{
+    private:
+        //Define p array and sBox for encryption 
+        vector<uint32_t> _pArray;
+        vector<vector<uint32_t> > _sBox;
+        uint32_t Ffunction(uint32_t data);
+
+    public:
+        Blowfish(vector<uint8_t> key);
+        void setKey(vector<uint8_t> key);
+        void encrypt(uint64_t & data);
+        void decrypt(uint64_t & data);
+};
